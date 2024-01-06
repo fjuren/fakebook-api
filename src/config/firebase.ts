05040a976a9c721a93252237b911aa1d27b6c4b1
Fabian Juren <fjuren@gmail.com>
2024-01-05 21:29:43 -0800

    resolve bug related to parsing the firebase_private_key

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -30,13 +30,13 @@ const storage = getStorage();
 // const analytics = getAnalytics(app);
 
 // need to initialize firebase-admin for creating a custom token
-// const serviceAccount = require('../config/serviceAccountKey.json');
+// const serviceAccount = require('./serviceAccountKey.json');
 admin.initializeApp({
   // credential: admin.credential.cert(serviceAccount),
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   }),
 });
 
