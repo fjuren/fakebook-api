530d313c16c52143200472321d0610e432677c8b
Fabian Juren <fjuren@gmail.com>
2024-01-10 21:09:22 -0800

    Try to fix firebase bug in production

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -36,7 +36,8 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    // privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // use when deploying from vsCode to Railway
+    privateKey: process.env.FIREBASE_PRIVATE_KEY, // use when deploying from github to railway
   }),
 });
 
